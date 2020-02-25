64ee8d5e4486ba083f9ffa0b19c32e237b80d0af
Dave Everitt <deveritt@innotts.co.uk>
2020-02-25 13:59:35 +0000

    change example to use insertAdjacentHTML

--- api-starwars/scripts.js
+++ api-starwars/scripts.js
@@ -18,13 +18,27 @@ function getFilms(films) {
   showData.innerText = "";
   films.forEach(listFilm);
   function listFilm(item, index) {
-    const title = document.createElement("dt");
-    const preamble = document.createElement("dd");
-    const content = document.createTextNode(`${item.episode_id}. ${item.title}`);
-    const words = document.createTextNode(item.opening_crawl);
-    title.appendChild(content);
-    preamble.appendChild(words);
-    showData.appendChild(title);
-    showData.appendChild(preamble);
+    const content = 
+      `<dt>${item.episode_id}. ${item.title}</dt>
+       <dd>${item.opening_crawl}</dd>`;
+    showData.insertAdjacentHTML("beforeend", content);
   }
 }
+// function getFilms(films) {
+//   films
+//     .sort((a, b) => a.episode_id - b.episode_id)
+//     .map(film => `${film.episode_id}. ${film.title}`);
+//   console.log(films);
+//   showData.innerText = "";
+//   films.forEach(listFilm);
+//   function listFilm(item, index) {
+//     const title = document.createElement("dt");
+//     const preamble = document.createElement("dd");
+//     const content = document.createTextNode(`${item.episode_id}. ${item.title}`);
+//     const words = document.createTextNode(item.opening_crawl);
+//     title.appendChild(content);
+//     preamble.appendChild(words);
+//     showData.appendChild(title);
+//     showData.appendChild(preamble);
+//   }
+// }
