6635c7eb0ce93dc51d62795e0f971b1075b226af
Dave Everitt <deveritt@innotts.co.uk>
2020-02-26 21:31:21 +0000

    add GitHub API example

---
+++ api-github/readme.md
@@ -0,0 +1,7 @@
+# An API demo using GitHub user details API
+
+You can use data from the [GitHub users API](https://api.github.com/users/) - append your GitHub user name to see the JSON API for your account. The data includes avatar images and much more. See the [GitHub REST API guide](https://developer.github.com/v3/).
+
+For unauthenticated requests, the [rate limit allows 60 requests per hour](https://developer.github.com/v3/#rate-limiting) (associated with the originating IP address) which might run out.
+
+To work with much higher limits, sign in to Github so you can [authenticate requests](https://developer.github.com/v3/auth/) to the GitHub API with an [API Personal access token](https://github.com/settings/tokens). You can also [check your current rate limit status](https://developer.github.com/v3/rate_limit/).
---
+++ api-github/scripts.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const API_URL = "https://api.github.com/users/";
+const gitHubUsers = ["fania", "daveeveritt", "ggstuart"]
+
+// user and token excluded from public repo!
+const creds = `${user}:${token}`;
+const auth = btoa(creds);
+
+const options = {
+  mode: 'cors',
+  headers: { 'Authorization': 'Basic ' + auth }
+}
+
+const getUserAsync = async (name) => {
+  // let response = await fetch(`${API_URL}${name}`, options);
+  let response = await fetch(`${API_URL}${name}`);
+  let data = await response.json();
+  return data;
+}
+
+const showUsers = (user) => {
+  const content = 
+   `<section>
+      <img src="${user.avatar_url}" alt="user.login">
+      <div>
+        <h2><a href="${user.html_url}">${user.login}</a></h2>
+        <ul>
+          <li>Company: ${user.company}</li>
+          <li>Public repos: ${user.public_repos}</li>
+          <li>Followers: ${user.followers}</li>
+          <li>Following: ${user.following}</li>
+        </ul>
+      </div>
+    </section>`;
+  showData.insertAdjacentHTML("beforeend", content);
+}
+
+showData.innerText = "";
+
+gitHubUsers.forEach( person => {
+getUserAsync(person)
+  .then(data => {
+    console.log(data);
+    showUsers(data);
+  })
+});
